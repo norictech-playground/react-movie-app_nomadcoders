8b15546b90f28724717132ab50781ed69b2a33c0
norictech <ahmad.uji1902@gmail.com>
2020-08-09 20:52:56 +0700

    Updates

---
+++ precache-manifest.cecbfe1e8bffd342ae14b59c6a103a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f223d14edddb75f17a8fec8d739ab5",
+    "url": "/react-movie-app_nomadcoders/index.html"
+  },
+  {
+    "revision": "811e789b613f0bf37a2a",
+    "url": "/react-movie-app_nomadcoders/static/css/main.ee2aaeaf.chunk.css"
+  },
+  {
+    "revision": "a8f964acf537abe88817",
+    "url": "/react-movie-app_nomadcoders/static/js/2.1154fc0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-app_nomadcoders/static/js/2.1154fc0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811e789b613f0bf37a2a",
+    "url": "/react-movie-app_nomadcoders/static/js/main.3d6e881c.chunk.js"
+  },
+  {
+    "revision": "4ca834cf5f10d48a8877",
+    "url": "/react-movie-app_nomadcoders/static/js/runtime-main.c05b83a8.js"
+  }
+]);
