672a6bb0a75aee033d7e86485e1187c4e443e5b3
norictech <ahmad.uji1902@gmail.com>
2020-08-09 16:12:09 +0700

    class component and state

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
-const foods = [
-  { name: "Chicken", },
-  { name: "Stick", },
-  { name: "Beaf", },
-  { name: "Noodle", },
-]
+class App extends React.Component {
+  state = {
+    count: 0
+  }
 
-function Food({ name }) {
-  return (
-    <h3>{ name }</h3>
-  )
-}
+  add = () => this.setState(current => ({ count: current.count + 1 })) // use current is a good pratice, current is equal to this.state
+  minus = () => this.setState(current => ({ count: current.count > 0 ? current.count - 1 : current.count }))
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired
-}
-
-const renderFood = (food) => {
-  return <Food key={ food.name } name={ food.name } />
-}
-
-function App() {
-  return (
-    <div>
-      { foods.map(renderFood) }
+  render() {
+    return <div>
+      <h3>Total of number is { this.state.count }</h3>
+      <button onClick={ this.add }>Add</button>
+      <button onClick={ this.minus }>Minus</button>
     </div>
-  );
+  }
 }
 
 export default App;
